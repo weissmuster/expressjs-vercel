0f718cb7bbaf9ab3c3920db275fecc7cc2e149c7
Thomas Wittig <weissmuster@gmail.com>
2022-02-07 23:00:52 +0100

    development

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
